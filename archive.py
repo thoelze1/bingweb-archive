030fd9f3dd551d0a4c762a4c5b38b31dc4ba1613
thoelze1 <thoelze1@binghamton.edu>
2017-02-23 22:39:23 -0500

    Slowly add all users

--- archive.py
+++ archive.py
@@ -1,7 +1,16 @@
+import os
 import subprocess
 
 def archive():
-  subprocess.call(["ls", "~/../", ">", "data/users2.txt"])
+  usersfile = open('data/users.txt', 'w+')
+  users = []
+  d = os.path.expanduser('~/../')
+  for o in os.listdir(d):
+    if os.path.isdir(os.path.join(d,o)):
+      os.path.join(d,o)
+      users.append((o+'\n'))
+  usersfile.writelines(users)
+  # subprocess.call(['ls',d,'>','data/users3.txt'])
 
 if __name__ == '__main__':
   archive()
